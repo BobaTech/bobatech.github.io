1f500e3a057bef322cef3a8137f58f472828d2db
cakesofwrath <dai.alan.dai@gmail.com>
2015-05-07 16:31:46 +0000

    more typin

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 	$(".code").typed({
 		strings: [
 			"function processData(data){\n\treturn data.map(function(row){\n\t\treturn [row.id, row.name, row.content];\n\t^500}\n^500}\nmodule.exports = processData;",
+			"public abstract class Socket {\n\tprotected Connection con;\n\tpublic abstract boolean connect(Connection c);\n}",
+			"import json\nwith open(\"data.json\", \"r\") as f:\n\tdata = json.loads(f)\nfor row in data:\n\tprint row",
+			"<?php\n$mysqli = mysqli_connect(\"kwaiwan.com\", \"admin\", \"1234\");\n$res = mysqli_query($mysqli, \"SELECT * FROM users\");\n$row = mysqli_fetch_assoc($res);\necho $row[\"_msg\"];\n?>",
 			"function Node(parent){\n\tthis.node = parent; \n}\n\nvar root = new Node(null);\nvar child = new Node(root);",
-			"import json\nwith open(\"data.json\", \"r\") as f:\n\tdata = json.loads(f)\nfor row in data:\n\tprint row"
 			],
 		contentType: "text",
 		typeSpeed: 1,
