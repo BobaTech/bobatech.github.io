f895f58141272fd479bae82a89eeab707c60e26b
CajunAzn7 <kevinw326@gmail.com>
2015-12-10 23:17:03 +0000

    extra stuff

--- js/tab.js
+++ js/tab.js
@@ -1,9 +1,11 @@
 var pages = ['home', 'projects', 'about', 'contact'];
 function switchTab(obj){
+    document.getElementById('cover').addClass = 'slide';
     for(var i = 0; i < pages.length; i++){
         if(obj != pages[i]){
             document.getElementById(pages[i]).style.display = "none";
         }
     }
     document.getElementById(obj).style.display = 'block';
+    document.getElementById('cover').removeClass = 'slide';
 }
