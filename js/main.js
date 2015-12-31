ef27207c3672dec9e5452b1a9ed26a4a5ac9d3eb
CajunAzn7 <kevinw326@gmail.com>
2015-12-31 02:12:29 +0000

    updated site

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var codes = [
 			"function processData(data){\n\treturn data.map(function(row){\n\t\treturn [row.id, row.name, row.content];\n\t^500}\n^500}\nmodule.exports = processData;",
 			"public abstract class Socket {\n\tprotected Connection con;\n\tpublic abstract boolean connect(Connection c);\n}",
 			"import json\nwith open(\"data.json\", \"r\") as f:\n\tdata = json.loads(f)\nfor row in data:\n\tprint row",
-			"<?php\n$mysqli = mysqli_connect(\"kwaiwan.com\", \"admin\", \"1234\");\n$res = mysqli_query($mysqli, \"SELECT * FROM users\");\n$row = mysqli_fetch_assoc($res);\necho $row[\"_msg\"];\n?>",
+			"<?php\n$mysqli = mysqli_connect(\"BobaTech.com\", \"admin\", \"1234\");\n$res = mysqli_query($mysqli, \"SELECT * FROM users\");\n$row = mysqli_fetch_assoc($res);\necho $row[\"_msg\"];\n?>",
 			"function Node(parent){\n\tthis.node = parent; \n}\n\nvar root = new Node(null);\nvar child = new Node(root);",
 			];
 			
